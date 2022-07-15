20cb150b6687e9dbb63fdc40a6a5775e269fdf43
sou-komatsu <sou-komastu@mahoujin.co.jp>
2022-07-15 17:14:18 +0900

    install material ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import RiggingValue from './component/riggingValue';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import IconButton from '@mui/material/IconButton';
+import Input from '@mui/material/Input';
+import FilledInput from '@mui/material/FilledInput';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+// import Visibility from '@mui/icons-material/Visibility';
+// import VisibilityOff from '@mui/icons-material/VisibilityOff';
+
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 function App() {
 	const [span, setSpan] = useState(159);
@@ -7,32 +29,64 @@ function App() {
 	const [inboard, setInboard] = useState(88);
 	const [gearingRatio, setGearingRation] = useState(0);
 	useEffect(() => {
-		setGearingRation((oarLength - inboard) / (span/2));
+		if (span <= 0) {
+			setGearingRation('');
+		} else {
+			setGearingRation((oarLength - inboard) / (span/2));
+		}	
 	})
-	const handleSpanChange = (data) => {
-		setSpan(data);
+	const handleSpanChange = (e) => {
+		setSpan(e.target.value);
 	}
-	const handleOarLengthChange = (data) => {
-		setOarLength(data);
+	const handleOarLengthChange = (e) => {
+		setOarLength(e.target.value);
 	}
-	const handleInboardChange = (data) => {
-		setInboard(data);
+	const handleInboardChange = (e) => {
+		setInboard(e.target.value);
 	}
 
 	return (
-		<div className="flex flex-col justify-center items-center w-screen h-screen bg-gray-900">
-			<div className="md:w-2/5 w-10/12">
+		<ThemeProvider theme={darkTheme}>
+			<CssBaseline />
+			<Container maxWidth="lg">
 				<h1 className="text-white text-3xl text-center mb-3 font-sans font-semibold">Gearing Ratio Calculator</h1>
-				<div className="flex flex-col rounded mx-auto bg-gray-500 px-6 py-8 w-full">
-					<RiggingValue onValueChange={handleSpanChange} value={span} label="Span" />
-					<RiggingValue onValueChange={handleOarLengthChange} value={oarLength} label="Oar Length" />
-					<RiggingValue onValueChange={handleInboardChange} value={inboard} label="Inboard" />
-				</div>
-				<h3 className="text-center lg:text-2xl md:text-lg text-base font-semibold text-white">
-						Gearing Ration: {gearingRatio}
+				<Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
+					<TextField
+						label="Span"
+						id="span"
+						sx={{ m: 1, width: '25ch' }}
+						value={span}
+						onChange={handleSpanChange}
+						InputProps={{
+							startAdornment: <InputAdornment position="start">cm</InputAdornment>,
+						}}
+						/>
+					<TextField
+						label="Oal Length"
+						id="oal-length"
+						sx={{ m: 1, width: '25ch' }}
+						value={oarLength}
+						onChange={handleOarLengthChange}
+						InputProps={{
+							startAdornment: <InputAdornment position="start">cm</InputAdornment>,
+						}}
+						/>
+					<TextField
+						label="Inboard"
+						id="inboard"
+						sx={{ m: 1, width: '25ch' }}
+						value={inboard}
+						onChange={handleInboardChange}
+						InputProps={{
+							startAdornment: <InputAdornment position="start">cm</InputAdornment>,
+						}}
+						/>
+				</Box>
+				<h3>
+					Gearing Ration: {gearingRatio}
 				</h3>
-			</div>
-		</div>
+			</Container>
+		</ThemeProvider>
 	);
 }
 
