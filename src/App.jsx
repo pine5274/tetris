3ff1f268ee13cb77afc72e336a5f347a384e069c
sou-komatsu <sou-komastu@mahoujin.co.jp>
2022-08-08 13:41:56 +0900

    サイドバー実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
 import CssBaseline from '@mui/material/CssBaseline';
+import Sidebar from './component/Sidebar';
 import GearRatio from './component/GearRatio';
-
 
 const darkTheme = createTheme({
   palette: {
@@ -30,19 +31,27 @@ const darkTheme = createTheme({
      	 main: '#d6bcfa',
     },
 	background: {
-		default: '#303030',
-		paper: '#424242'
+		default: '#313545',
+		paper: '#3b4252'
 	},
   },
 });
 
 function App() {
+	const drawerWidth = 240;
 	return (
 		<ThemeProvider theme={darkTheme}>
-			<CssBaseline />
-			<Container maxWidth="lg">
-				<GearRatio />
-			</Container>
+			<Box sx={{ display: 'flex' }}>
+				<CssBaseline />
+				<Sidebar drawerWidth={drawerWidth} />
+				<Box
+					component="main"
+					sx={{ flexGrow: 1, p: 3, width: { lg: `calc(100% - ${drawerWidth}px)` } }}
+					>
+					<Toolbar />
+					<GearRatio />
+				</Box>	
+			</Box>
 		</ThemeProvider>
 	);
 }
