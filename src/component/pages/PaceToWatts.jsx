49601db7abe4f96907192162859acac3972eae70
sou-komatsu <sou-komastu@mahoujin.co.jp>
2022-08-22 11:03:26 +0900

    add pace to watts

--- src/component/pages/PaceToWatts.jsx
+++ src/component/pages/PaceToWatts.jsx
@@ -1,55 +1,155 @@
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box'
-import Paper from '@mui/material/Paper';
 import TextField from '@mui/material/TextField';
 import Divider from '@mui/material/Divider';
 
-const PaceToWatts = () => {
+function TabPanel(props) {
+	const { children, value, index, ...other } = props;
+  
+	return (
+		<div
+			role="tabpanel"
+			hidden={value !== index}
+			id={`simple-tabpanel-${index}`}
+			aria-labelledby={`simple-tab-${index}`}
+			{...other}
+		>
+			{value === index && (
+			<Box sx={{ p: 3 }}>
+				{children}
+			</Box>
+			)}
+		</div>
+	);
+}
+  
+TabPanel.propTypes = {
+	children: PropTypes.node,
+	index: PropTypes.number.isRequired,
+	value: PropTypes.number.isRequired,
+};
+
+const PaceTo = () => {
     const [minutes, setMinutes] = useState(1);
     const [seconds, setSeconds] = useState(45);
     const [tenths, setTenths] = useState(0);
+    const [watts, setWatts] = useState(0);
   
     const handleMinutesChange = (e) => {
-        setMinutes(e.target.value);
+        setMinutes(Number(e.target.value));
     }
     const handleSecondsChange = (e) => {
-        setSeconds(e.target.value);
+        setSeconds(Number(e.target.value));
     }
     const handleTenthsChange = (e) => {
-        setTenths(e.target.value);
+        setTenths(Number(e.target.value));
     }
+
+    useEffect(() => {
+        setWatts(2.80 / ((minutes * 60 + seconds + tenths * 0.1) / 500) ** 3);
+    })
+
     return (
         <>
-            <h1>Pace to Watt Calculator</h1>
+            <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
+                <TextField
+                    label="Minutes"
+                    id="minutes"
+                    sx={{ m: 1, width: '25ch' }}
+                    value={minutes}
+                    type="tel"
+                    onChange={handleMinutesChange}
+                />
+                <TextField
+                    label="Seconds"
+                    id="seconds"
+                    sx={{ m: 1, width: '25ch' }}
+                    value={seconds}
+                    type="tel"
+                    onChange={handleSecondsChange}
+                />
+                <TextField
+                    label="Tenths"
+                    id="tenths"
+                    sx={{ m: 1, width: '25ch' }}
+                    value={tenths}
+                    type="tel"
+                    onChange={handleTenthsChange}
+                />
+            </Box>
+            <Divider />
+            <h2>{watts}</h2>
+        </>
+    )
+}
+
+const WattsTo = () => {
+    const [minutes, setMinutes] = useState(1);
+    const [seconds, setSeconds] = useState(45);
+    const [watts, setWatts] = useState(200);
+  
+    const handleWattsChange = (e) => {
+        setWatts(Number(e.target.value));
+    }
+
+    useEffect(() => {
+        let s = 500 * (2.8 / watts) ** (1/3);
+        console.log(s);
+        setMinutes(Math.floor(s / 60));
+        s = Math.round(Math.floor((s % 60) * 10))/10;
+        if ( s < 1 ) {
+            s = "0" + String(s);
+        }
+        setSeconds(s);
+    })
+
+    return (
+        <>
+            <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
+                <TextField
+                    label="Watts"
+                    id="watts"
+                    sx={{ m: 1, width: '25ch' }}
+                    value={watts}
+                    type="tel"
+                    onChange={handleWattsChange}
+                />
+            </Box>
+            <Divider />
+            <h2>{minutes}:{seconds}/500m</h2>
+        </>
+    )
+}
+
+const PaceToWatts = () => {
+    const [tabNum, setTabNum] = React.useState(0);
+    const handleTabChange = (event, newValue) => {
+        setTabNum(newValue);
+    };
+
+    return (
+        <>
+            <h1>Pace to Watts Calculator</h1>
             <Box sx={{ width: '100%' }}>
-                <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
-                    <TextField
-                        label="Minutes"
-                        id="minutes"
-                        sx={{ m: 1, width: '25ch' }}
-                        value={minutes}
-                        type="tel"
-                        onChange={handleMinutesChange}
-                    />
-                    <TextField
-                        label="Seconds"
-                        id="seconds"
-                        sx={{ m: 1, width: '25ch' }}
-                        value={seconds}
-                        type="tel"
-                        onChange={handleSecondsChange}
-                    />
-                    <TextField
-                        label="Tenths"
-                        id="tenths"
-                        sx={{ m: 1, width: '25ch' }}
-                        value={tenths}
-                        type="tel"
-                        onChange={handleTenthsChange}
-                    />
+                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                <Tabs
+                    value={tabNum}
+                    onChange={handleTabChange}
+                    aria-label="secondary tabs example"
+                >
+                    <Tab value={0} label="Pace to Watt" />
+                    <Tab value={1} label="Watt to Pace" />
+                </Tabs>
                 </Box>
-                <Divider />
-                <h2>200watts</h2>
+                <TabPanel value={tabNum} index={0}>
+                    <PaceTo />
+                </TabPanel>
+                <TabPanel value={tabNum} index={1}>
+                    <WattsTo />
+                </TabPanel>
             </Box>
         </>
     )
